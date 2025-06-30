fb148f9efd9fdfb94c98bde593c3b93ec79a8385
Efim Kubishkin <efimkub@mail.ru>
2025-06-30 15:26:31 +0300

    Add common words and imply via LUT example

---
+++ src/04-mux-demux/lut_imply.sv
@@ -0,0 +1,11 @@
+module lut_imply (
+  input logic a, b,
+  output logic c
+);
+  mux_4to1 lut(
+    .in({1'b1, 1'b1, 1'b0, 1'b0}),
+    .sel({a, b}),
+    .out(c)
+  );
+
+endmodule
--- src/04-mux-demux/mux_2to1_logic.sv
+++ src/04-mux-demux/mux_2to1_logic.sv
@@ -8,29 +8,3 @@ module mux_2to1_logic (
 
 
 endmodule
-
-// module simple_if_tb;
-
-// logic clk, b, a;
-// simple_if dut(
-//   .clk(clk),
-//   .a(a),
-//   .b(b));
-//   initial begin
-//     clk <= '0;
-//     forever begin
-//       #5;
-//       clk <= ~clk;
-//     end
-//   end
-//   initial begin
-//     $monitor("clk=%d, a=%d, b=%d", clk, a, b);
-//     @(posedge clk) a = 0;
-//     @(posedge clk) a = 0;
-//     @(posedge clk) a = 1;
-//     @(posedge clk) begin
-//       a = 1;
-//       $finish();
-//     end
-//   end
-// endmodule
