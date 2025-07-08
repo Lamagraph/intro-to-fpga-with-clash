08a4c64f9b2710ba276d4cf88f6e99bbb0b8e156
Efim Kubishkin <efimkub@mail.ru>
2025-07-08 13:35:08 +0300

    Add renumerate of broken links

--- renumerate.py
+++ renumerate.py
@@ -3,10 +3,34 @@ import sys
 import subprocess
 
 
+def renumerate_link(path):
+    number = ""
+    left, right = "", ""
+    for i in range(len(path) - 1):
+        pair = path[i : i + 2]
+        if str.isnumeric(pair):
+            number = ("0" + str(int(pair) + 1))[-2:]
+            left = path[:i]
+            right = path[i + 2 :]
+            break
+    return left + number + right
+
+
 def renumerate(file_path: str) -> str:
     number, other_name = file_path[:2], file_path[2:]
     next_number = ("0" + str(int(number) + 1))[-2:]
     return next_number + other_name
+
+
+def renumerate_broken_links():
+    for root, _, files in os.walk("."):
+        for name in files:
+            path = os.path.join(root, name)
+            if os.path.islink(path) and not os.path.exists(path):
+                link = os.readlink(path)
+                renumerated_link = renumerate_link(os.readlink(path))
+                print(f"{link} -> {renumerated_link}")
+                subprocess.run(["ln", "-sf", renumerated_link, path])
 
 
 def renumerate_modules(directory_path, start_module):
@@ -37,3 +61,4 @@ def renumerate_modules(directory_path, start_module):
 
 
 renumerate_modules(sys.argv[1], sys.argv[2])
+renumerate_broken_links()
