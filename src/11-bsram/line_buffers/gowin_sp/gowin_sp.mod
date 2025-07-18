4448df1437c1b2883fa58ee95d1646d13c5ff918
Efim Kubishkin <efimkub@mail.ru>
2025-07-18 13:30:28 +0300

    Add code for line buffer

---
+++ src/11-bsram/line_buffers/gowin_sp/gowin_sp.mod
@@ -0,0 +1,17 @@
+-series GW5AST
+-device GW5AST-138
+-device_version B
+-package FCPBGA676A
+-part_number GW5AST-LV138FPG676AES
+
+
+-mod_name Gowin_SP
+-file_name gowin_sp
+-path /home/efim/Documents/intro-to-fpga-with-clash/src/11-bsram/line_buffers/gowin_sp/
+-type RAM_SP
+-file_type vlg
+-depth 640
+-width 8
+-read_mode pipeline
+-write_mode normal
+-reset_mode sync
---
+++ src/11-bsram/line_buffers/line_buffer.sv
@@ -0,0 +1,82 @@
+`timescale 1ns / 1ps // <1>
+module line_buffer #(
+  parameter int IMAGE_WIDTH = 640,
+  parameter int PIXEL_WIDTH = 8
+)(
+  input  logic clk,
+  input  logic rst,
+  input  logic [PIXEL_WIDTH - 1:0] pixel_in,
+  output logic [PIXEL_WIDTH - 1:0] pixel_out,
+  output logic ready_to_write // <2>
+);
+
+logic wre;
+logic[1:0] state;
+logic[9:0] address;
+logic ce = 1;
+logic oce = 1;
+
+
+parameter FILLING = 0; // <3>
+parameter WRITE = 1;
+parameter READ = 2;
+
+logic [PIXEL_WIDTH-1:0] din;
+
+always_ff @(posedge clk) begin
+  if (rst) begin
+    address <= 0;
+    state <= FILLING;
+    wre <= 1;
+    din <= pixel_in;
+    ready_to_write <= 1;
+  end
+end
+
+Gowin_SP sp( // <4>
+  .dout(pixel_out),
+  .clk(clk),
+  .oce(oce),
+  .ce(ce),
+  .reset(rst),
+  .wre(wre),
+  .ad(address),
+  .din(din)
+);
+
+
+
+always_ff @(posedge clk) begin
+  case(state)
+    FILLING: // <5>
+    begin
+      din <= pixel_in;
+      if (address == IMAGE_WIDTH - 1) begin
+        state <= READ;
+        address <= 10'd0;
+        wre <= 0;
+        ready_to_write <= 0;
+      end
+      else address <= address + 10'd1;
+    end
+    READ: // <6>
+    begin
+      state <= WRITE;
+      wre <= 1;
+      ready_to_write <= 1;
+    end
+    WRITE: // <7>
+    begin
+      state <= READ;
+      wre <= 0;
+      ready_to_write <= 0;
+      din <= pixel_in;
+      if (address == IMAGE_WIDTH - 1) begin
+        address <= 10'd0;
+      end
+      else address <= address + 10'd1;
+    end
+  endcase
+end
+
+endmodule
