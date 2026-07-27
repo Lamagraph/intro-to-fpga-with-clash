05c11c68702b1b134b8e458d049654c8ac51d3c4
Timofeev Matvey <nomeow228@gmail.com>
2026-07-27 23:11:30 +0300

    Fix serial-to-parallel cocotb test again (8 module)
    * Clock() parameter renamed from 'units' to 'unit' in cocotb 2.0.1
    
    * Previously, model incremented counter and then reset it to 0,
      when it equaled 'OutWidth - 1', and assertion condition 'counter ==
      OutWidth - 1' never became true.
      Fix this: now model resets counter to 0, when it equals 'OutWidth',
      and then increments it. And change assertion condition from
      'counter == OutWidth - 1' to 'counter == OutWidth'
    
    * Change assertion 'assert dut.m_valid' to 'assert
      int(dut.m_valid.value)' for correct type casting
    
    * Add assertion 'assert not int(dut.m_valid.value)', when 'counter !=
      OutWidth' to avoid false green status on the check

--- src/08-cocotb/bus/bus_test.py
+++ src/08-cocotb/bus/bus_test.py
@@ -37,16 +37,16 @@ class HelperSerialParallel:  # <1>
                 self.res = [int(self.dut.s_data.value)] + self.res[
                     0 : self.OutWidth - 1
                 ]
-                self.counter += 1
-                if self.counter == self.OutWidth - 1:
+                if self.counter == self.OutWidth:
                     self.counter = 0
+                self.counter += 1
 
 
 @cocotb.test()
 async def bus_test(dut):
     NOfIterations = 1000
 
-    clock = Clock(dut.clk, 10, units="ns")  # <5>
+    clock = Clock(dut.clk, 10, unit="ns")  # <5>
     helper = HelperSerialParallel(dut)
     cocotb.start_soon(clock.start(start_high=False))  # <6>
 
@@ -62,8 +62,10 @@ async def bus_test(dut):
         await RisingEdge(dut.clk)
 
         cocotb.start_soon(helper.my_serial_to_parallel())
-        if helper.counter == helper.OutWidth - 1:
-            assert dut.m_valid, f"Incorrect m_valid = {dut.m_valid.value}"
+        if helper.counter == helper.OutWidth:
+            assert int(dut.m_valid.value), f"Incorrect m_valid = {dut.m_valid.value}"
             assert (
                 LogicArray(helper.res) == dut.m_data.value  # <8>
             ), f"m_data = {dut.m_data.value}, res = {LogicArray(helper.res)}"
+        else:
+            assert not int(dut.m_valid.value), f"Incorrect m_valid = {dut.m_valid.value}"
